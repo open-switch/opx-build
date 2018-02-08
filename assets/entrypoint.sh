4525dd20a7b32d8e5839255df0d07bdec612f208
Tyler Heucke <tyler.heucke@dell.com>
2018-02-08 11:46:33 -0800

    Add opx user explicitly to sudoers file
    On macOS, group ids differ from inside the container. This change fixes
    password-less root access on macOS.
    
    Change-Id: I12d7f6bb03cc1a1a17f051b6ca3d654bb9cce587
    Signed-off-by: Tyler Heucke <tyler.heucke@dell.com>

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -6,6 +6,7 @@ if [ -n "${LOCAL_UID}" ]; then
     if ! grep -q opx /etc/group; then
         groupadd -o --gid=${LOCAL_GID} opx
         useradd -o --uid=${LOCAL_UID} --gid=${LOCAL_GID} -s /bin/bash opx
+        echo 'opx ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
         echo '%opx ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
     fi
 
